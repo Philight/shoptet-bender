a9343dea2b06b6015c4a2340e8daaad713ceb676
Philight <Philight@users.noreply.github.com>
2024-09-16 20:31:42 +0200

    Basic structure

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # Shoptet Bender 🤖
 
 ## Introduction
+
 Shoptet Bender proxies remote e-shop to localhost while injecting and serving your local `style.css` and `script.js`. This tool enables to development of visual changes without breaking the production e-shop. It is also suitable for Premium e-shop development, while emulation of the Blank mode is possible.
 
 ## How it works
@@ -12,17 +13,20 @@ C(Local files) -- Static served files --> B
 ```
 
 ## Install
+
 Node >= 14 prerequisited\
 Install global using yarn:\
 `yarn global add git+https://github.com/shoptet/shoptet-bender.git`
 
 ℹ️ On Windows platform, you may need to add Yarn binary to your path:
-- copy the output of the `yarn global bin` command, e.g. *C:\Users\YOUR_PROFILE\AppData\Local\Yarn\bin*
+
+- copy the output of the `yarn global bin` command, e.g. _C:\Users\YOUR_PROFILE\AppData\Local\Yarn\bin_
 - open "**System properties**" and click on the "**Environment Variables**" button
 - select "**Path**", click on "**Edit**" -> "**New**", paste the copied output and save
 - you need to open a new instance of the CLI interface to take an effect
 
 ## Usage
+
 ### Step-by-step guide to start
 
 1. Create a folder for your files (css, js) that need to be injected to remote e-shop on localhost (let's say "my-project")
@@ -36,9 +40,20 @@ And you're ready to go -> enjoy coding and development ;)
 
 Try `shp-bender -h` for CLI help
 
+## Theme development
+
+Theme templates are located in `/assets` folder
+
+> [!NOTE]  
+> For theme development guidance open README in `/theme/README.md`
+
 ## Posible tool improvements
+
 Shoptet Bender is an open-source project, so your PRs are very welcomed. There are some suggestions for possible features, as Shoptet don't have the resources to develop this tool actively. So it is left as is for your active development. Some minor feature updates are possible.
 What could be done:
+
 - [ ] tools integration (Gulp, Grunt)
+  - [ ] Gulp
+  - [x] Grunt
 - [ ] files concatenation
 - [ ] HEAD and BODY scripts location - this could better emulate admin fields
---
+++ theme/js/testing.js
@@ -0,0 +1,77 @@
+(function () {
+  document.addEventListener("DOMContentLoaded", function () {
+    console.info("DOMContentLoaded");
+    console.info("shoptet", shoptet);
+
+    let selector = "";
+
+    /**
+     * Custom fn for iterating Nodes
+     */
+    var forEachElement = function (array, callback, scope) {
+      for (var i = 0; i < array.length; i++) {
+        callback.call(scope, i, array[i]); // passes back stuff we need
+      }
+    };
+
+    //     const url = "https://673800.myshoptet.com/api/";
+
+    //     // const url =
+    //     //   "https://673800.myshoptet.com/action/ApiOAuthServer/getAccessToken";
+
+    //     fetch(url)
+    //       .then((res) => console.log(res))
+    //       .catch((e) => console.error(e));
+
+    //     //
+    //     $("body").append(`
+    //      <h3>
+    //          This is the text which has
+    //          been inserted by JS
+    //      </h3>
+
+    //      <form action="">
+    //        <input type="text" name="query" id="searchTesting" value="box" />
+    //      </form>
+
+    //      <div id="boxTesting" style="width: 200px; height: 200px; border: 1px solid red">
+    // BOX INSTER HERE
+    //      </div>
+
+    //  `);
+
+    //     let $searchInput = $("#searchTesting");
+    //     let $searchContainer = $("#boxTesting");
+    //     var xhr;
+
+    //     function ajaxTesting() {
+    //       var $form = $searchInput.parents("form");
+    //       if (!xhr || xhr.readyState === 4) {
+    //         console.log("ajax form", $form.serialize());
+    //         xhr = $.ajax({
+    //           url: "/action/ProductSearch/ajaxSearch/",
+    //           type: "GET",
+    //           headers: { "X-Shoptet-XHR": "Shoptet_Coo7ai" },
+    //           data: $form.serialize(),
+    //         })
+    //           .done(function (result) {
+    //             console.log("ajax result", result);
+    //             var response = $.parseJSON(result);
+    //             console.log("ajax result JSON", response);
+    //             // $searchContainer.html(response);
+    //             // if ($searchInput.is(":focus")) {
+    //             //   $searchContainer.addClass("active");
+    //             //   $("body").addClass("search-focused");
+    //             // }
+    //             shoptet.scripts.signalDomLoad("ShoptetDOMSearchResultsLoaded");
+    //           })
+    //           .fail(function () {
+    //             // TODO: add error message
+    //           });
+    //       }
+    //     }
+
+    //     ajaxTesting();
+    //     fulltextSearch($searchInput, $searchContainer);
+  });
+})();
